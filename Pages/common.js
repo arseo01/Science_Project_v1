e9a0631f78e97916cd7a669ac83b9cf976a2b997
arseo01 <arseoazemaj@gmail.com>
2026-02-03 13:25:37 +0100

    shity work

--- Pages/common.js
+++ Pages/common.js
@@ -1,17 +1,17 @@
 const menu_page = document.getElementById("menu");
-const blur = document.getElementById("background_blur");
+const background_blur = document.getElementById("background_blur");
 
 function open_menu() {
     menu_page.classList.add("show");
     menu_page.classList.remove("hide");
-    blur.style.visibility = "visible";
+    background_blur.style.visibility = "visible";
 }
 
 function blur_background() {
     setTimeout(() => {
         menu_page.classList.add("hide");
         menu_page.classList.remove("show");
-        blur.style.visibility = "hidden";
+        background_blur.style.visibility = "hidden";
     }, 100);
 }
 
